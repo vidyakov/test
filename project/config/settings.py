cd7babdd5454ad00bbd74aa56e675c5de3b54cfe
vidyakov <sashavidyakov2000@gmail.com>
2020-08-12 16:35:02 +0300

    -

--- project/config/settings.py
+++ project/config/settings.py
@@ -9,10 +9,15 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4i28zv1cz45ap(6p0lq4h!*h$+e*z1t$n7s08n*v3(*i6-8s4z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,19 +128,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.vk.VKOAuth2',
 )
 
-SOCIAL_AUTH_VK_OAUTH2_KEY = '7564017'
-SOCIAL_AUTH_VK_OAUTH2_SECRET = 'TX6wWWc8dup33PD8ffGz'
+SOCIAL_AUTH_VK_OAUTH2_KEY = os.environ.get('VK_KEY')
+SOCIAL_AUTH_VK_OAUTH2_SECRET = os.environ.get('VK_SECRET')
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
